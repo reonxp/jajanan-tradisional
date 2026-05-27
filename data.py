e4d4e86ba8d5db2b499b205bfe9bccd9aa09b916
Reon <bagusdzakiy19@gmail.com>
2026-05-27 23:35:54 +0700

    fix scan

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 # ==========================================
 # DATABASE & KONFIGURASI JAJANAN TRADISIONAL
+# KOTA ASAL / KHAS DI JAWA TIMUR
 # ==========================================
 
-# Urutan sesuai persis dengan list yang lu kasih
 CLASS_NAMES = [
     'bagiak', 'brem', 'cenil', 'coro bikang', 'dadar gulung', 
     'gethuk lindri', 'gethuk pisang', 'kembang goyang', 'klepon', 'kue cucur', 
@@ -19,31 +19,31 @@ JAJANAN_DB = {
     },
     'brem': {
         'nama_display': 'Brem',
-        'asal': 'Madiun / Wonogiri, Jawa Timur & Jawa Tengah',
+        'asal': 'Madiun, Jawa Timur',
         'deskripsi': 'Makanan padat hasil fermentasi sari ketan hitam yang dikeringkan. Jajanan ini memiliki sensasi unik berupa rasa asam-manis kuat yang langsung memberikan rasa "dingin" dan meleleh seketika saat diemut di atas lidah.',
         'bahan': 'Sari tape ketan hitam, ragi fermentasi.'
     },
     'cenil': {
         'nama_display': 'Cenil',
-        'asal': 'Jawa Tengah / Jawa Timur',
+        'asal': 'Surabaya, Jawa Timur',
         'deskripsi': 'Jajanan pasar bertekstur sangat kenyal yang dibentuk bulat kecil atau lonjong beraneka warna cerah. Disajikan dengan taburan kelapa parut melimpah dan disiram kuah kinca (gula merah cair).',
         'bahan': 'Tepung sagu/tapioka, air panas, pewarna makanan, kelapa parut, gula merah.'
     },
     'coro bikang': {
         'nama_display': 'Coro Bikang',
-        'asal': 'Jawa Tengah / Jawa Timur',
+        'asal': 'Surabaya, Jawa Timur',
         'deskripsi': 'Kue basah berbahan dasar tepung beras yang dimasak di dalam cetakan khusus panas hingga bagian atasnya merekah mekar menyerupai bunga. Memiliki tekstur serat-serat empuk dengan rasa manis gurih santan.',
         'bahan': 'Tepung beras, santan kelapa, gula pasir, tepung terigu, pewarna makanan.'
     },
     'dadar gulung': {
         'nama_display': 'Dadar Gulung',
-        'asal': 'Pulau Jawa',
+        'asal': 'Malang, Jawa Timur',
         'deskripsi': 'Kue basah berupa dadar tipis berbahan terigu berwarna hijau alami (dari daun suji/pandan) yang digulung dengan isian unti kelapa, yaitu parutan kelapa muda yang dimasak manis bersama gula merah.',
         'bahan': 'Tepung terigu, santan, telur, air daun suji, kelapa parut, gula merah.'
     },
     'gethuk lindri': {
         'nama_display': 'Gethuk Lindri',
-        'asal': 'Magelang, Jawa Tengah',
+        'asal': 'Kediri, Jawa Timur',
         'deskripsi': 'Olahan singkong kukus yang dihaluskan bersama gula, diberi warna-warni menarik, kemudian digiling menggunakan cetakan hingga membentuk guratan serat memanjang lalu dipotong kotak. Biasa disajikan bersama parutan kelapa gurih.',
         'bahan': 'Singkong berkualitas, gula pasir, margarin, vanili, pewarna makanan, kelapa parut.'
     },
@@ -55,80 +55,80 @@ JAJANAN_DB = {
     },
     'kembang goyang': {
         'nama_display': 'Kembang Goyang',
-        'asal': 'Betawi / DKI Jakarta',
+        'asal': 'Gresik, Jawa Timur',
         'deskripsi': 'Kue kering renyah berbentuk kelopak bunga matahari. Proses pembuatannya sangat ikonik, yaitu cetakan besi dicelupkan ke adonan encer, dimasukkan ke minyak panas, lalu digoyang-goyang hingga adonan terlepas dan matang garing.',
         'bahan': 'Tepung beras, santan, telur, gula pasir, garam, biji wijen.'
     },
     'klepon': {
         'nama_display': 'Klepon',
-        'asal': 'Pulau Jawa & Bali',
-        'deskripsi': 'Kue tradisional berbentuk bola-bola kecil berwarna hijau kenyal yang dilapisi kelapa parut. Ciri khas utamanya adalah sensasi "ledakan" cairan gula merah hangat di dalam mulut saat digigit.',
+        'asal': 'Pasuruan, Jawa Timur',
+        'deskripsi': 'Kue tradisional berbentuk bola-bola kecil berwarna hijau kenyal yang dilapisi kelapa parut. Ciri khas utamanya adalah sensasi "ledakan" cairan gula merah hangat di dalam mulut saat digigit (terkenal di daerah Gempol, Pasuruan).',
         'bahan': 'Tepung ketan, air daun pandan/suji, gula merah (isian), kelapa parut kasar.'
     },
     'kue cucur': {
         'nama_display': 'Kue Cucur',
-        'asal': 'Betawi / Jawa Barat',
+        'asal': 'Sidoarjo, Jawa Timur',
         'deskripsi': 'Jajanan goreng berbentuk bulat pipih dengan bagian tengah tebal empuk dan pinggiran tipis garing berenda (*crispy*). Memiliki aroma karamel wangi yang pekat karena penggunaan gula aren asli.',
         'bahan': 'Tepung beras, tepung terigu, gula merah/aren, air, daun pandan.'
     },
     'kue lapis': {
         'nama_display': 'Kue Lapis',
-        'asal': 'Pulau Jawa',
+        'asal': 'Surabaya, Jawa Timur',
         'deskripsi': 'Kue basah bertekstur kenyal kenyal menggemaskan yang dibuat dari tepung beras dan santan. Dimasak secara sabar dengan mengukus adonan selapis demi selapis secara bergantian warna, sehingga tiap lapisannya bisa dikelupas.',
         'bahan': 'Tepung beras, tepung sagu, santan kelapa, gula pasir, garam, pewarna makanan.'
     },
     'kue lumpur': {
         'nama_display': 'Kue Lumpur',
-        'asal': 'Jakarta / Pulau Jawa',
-        'deskripsi': 'Kue basah tradisional bertekstur sangat lembut, basah, dan lumer layaknya lumpur. Dimasak di atas cetakan semi-datar dengan ciri khas toping kismis atau serutan kelapa muda di bagian tengahnya.',
+        'asal': 'Sidoarjo, Jawa Timur',
+        'deskripsi': 'Kue basah tradisional bertekstur sangat lembut, basah, dan lumer layaknya lumpur. Sangat legendaris di Sidoarjo dengan ciri khas toping kismis atau serutan kelapa muda di bagian tengahnya.',
         'bahan': 'Kentang kukus halus, tepung terigu, santan, telur, margarin cair, gula pasir, kismis.'
     },
     'kue perut ayam': {
         'nama_display': 'Kue Perut Ayam',
-        'asal': 'Jawa Timur',
+        'asal': 'Malang, Jawa Timur',
         'deskripsi': 'Jajanan goreng tradisional yang bentuknya melingkar-lingkar spiral tidak beraturan mirip seperti usus atau perut ayam. Memiliki tekstur empuk di dalam dan sedikit renyah di luar dengan rasa manis gurih tape.',
         'bahan': 'Tepung terigu, tape singkong haluskan, telur, santan, gula pasir, baking powder.'
     },
     'kue tok': {
         'nama_display': 'Kue Tok (Kue Ku)',
-        'asal': 'Akulturasi Tionghoa-Nusantara',
+        'asal': 'Surabaya, Jawa Timur',
         'deskripsi': 'Kue basah kenyal dari tepung ketan berbentuk menyerupai tempurung kura-kura dengan motif cetakan tradisional, umumnya berwarna merah menyala. Diisi dengan pasta kacang hijau kupas yang padat manis.',
         'bahan': 'Tepung ketan, kentang halus (untuk kulit), pasta kacang hijau, gula, minyak kelapa.'
     },
     'madu mongso': {
         'nama_display': 'Madu Mongso',
-        'asal': 'Ponorogo / Kediri, Jawa Timur',
+        'asal': 'Ponorogo, Jawa Timur',
         'deskripsi': 'Jajanan kuno bertekstur kenyal, berserat, dan legit mirip dodol. Terbuat dari olahan tape ketan hitam yang dimasak lama bersama santan dan gula. Secara tradisional dibungkus kecil-kecil menggunakan kertas minyak warna-warni.',
         'bahan': 'Tape ketan hitam matang, santan kental, gula merah, gula pasir.'
     },
     'mendhut': {
         'nama_display': 'Mendhut',
-        'asal': 'Jawa Tengah / Jawa Timur',
+        'asal': 'Blitar, Jawa Timur',
         'deskripsi': 'Kue basah berbentuk bulat kenyal dari tepung ketan berisi unti kelapa manis. Keunikannya terletak pada siraman kuah santan kental (kanil) yang gurih pekat, kemudian dibungkus daun pisang berbentuk takir kerucut.',
         'bahan': 'Tepung ketan, pewarna makanan, kelapa parut, gula merah, santan kental instan (kuah).'
     },
     'onde-onde': {
         'nama_display': 'Onde-onde',
         'asal': 'Mojokerto, Jawa Timur',
-        'deskripsi': 'Kue bulat goreng legendaris berbalut biji wijen di seluruh permukaannya. Memiliki kombinasi tekstur renyah garing di luar dan kenyal di dalam dengan isian kacang hijau tumbuk yang manis lembut.',
+        'deskripsi': 'Kue bulat goreng legendaris berbalut biji wijen di seluruh permukaannya. Kota Mojokerto sendiri sangat terkenal dijuluki sebagai Kota Onde-Onde karena kelestarian jajanan ini.',
         'bahan': 'Tepung ketan, biji wijen, kacang hijau kupas, gula pasir, santan kelapa.'
     },
     'pukis': {
         'nama_display': 'Pukis',
-        'asal': 'Banyumas, Jawa Tengah',
+        'asal': 'Surabaya, Jawa Timur',
         'deskripsi': 'Kue basah empuk bertekstur mirip sponge cake yang dimasak menggunakan cetakan baris setengah lingkaran. Bagian bawahnya berwarna cokelat gurih, sementara bagian atasnya lembut berongga dengan variasi topping cokelat atau keju.',
         'bahan': 'Tepung terigu, telur, gula pasir, ragi instan, santan hangat, margarin cair.'
     },
     'putu ayu': {
         'nama_display': 'Putu Ayu',
-        'asal': 'Pulau Jawa',
+        'asal': 'Malang, Jawa Timur',
         'deskripsi': 'Kue basah kukus berbentuk bunga cetakan yang sangat empuk dan ringan. Memiliki aroma pandan wangi berwarna hijau cerah yang berpadu kontras dengan gurihnya lapisan kelapa parut asin di atasnya.',
         'bahan': 'Tepung terigu, telur ayam, gula pasir, santan, air pandan, kelapa parut setengah tua.'
     },
     'wingko babat': {
         'nama_display': 'Wingko Babat',
-        'asal': 'Babat (Lamongan), Jawa Timur',
-        'deskripsi': 'Kue panggang tradisional berbentuk bundar pipih padat. Memiliki tekstur agak liat kenyal yang didominasi rasa manis gurih dari kelapa parut muda melimpah, dipadu wangi gosong panggangan yang menggugah selera.',
+        'asal': 'Lamongan, Jawa Timur',
+        'deskripsi': 'Kue panggang tradisional berbentuk bundar pipih padat. Berasal asli dari Kecamatan Babat di Lamongan. Memiliki tekstur agak liat kenyal yang didominasi rasa manis gurih dari kelapa parut muda melimpah.',
         'bahan': 'Tepung ketan, kelapa muda parut, santan, gula pasir, telur.'
     }
 }
